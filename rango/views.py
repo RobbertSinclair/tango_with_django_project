00d5a925e165f5bf89605de3f138ad396f4efce6
Robbert Sinclair <christanina57@gmail.com>
2021-01-17 22:40:38 +0000

    Made changes to help the code pass the tests

--- rango/views.py
+++ rango/views.py
@@ -4,18 +4,16 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     #Context for the template
-    context_dict = {"boldmessage": "Cool message"}
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     
-
-
-    return render(request, "rango/index.html", context=context_dict)
+    return render(request, 'rango/index.html', context=context_dict)
 
 def rango_app(request):
     return HttpResponse("Rango says wow you are in the rango app :)")
 
 def rango_about(request):
 
-    context_dict = {"context_about": "Rango is a website I am making for WAD2", "name": "Robbert Sinclair"}
-    return render(request, "rango/about.html", context=context_dict)
+    context = {"name": "Robbert Sinclair"}
+    return render(request, "rango/about.html", context=context)
 
 
