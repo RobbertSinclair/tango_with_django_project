951c973d56447faba28c9a76c3829ca143a35c32
Robbert Sinclair <christanina57@gmail.com>
2021-01-20 23:41:14 +0000

    Finished Chapter 5

--- rango/admin.py
+++ rango/admin.py
@@ -3,4 +3,11 @@ from rango.models import Category, Page
 
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Page)
+
+
+class PageAdmin(admin.ModelAdmin):
+    fields = ['title', 'category', 'url']
+    list_display = ('title', 'category', 'url')
+
+
+admin.site.register(Page, PageAdmin)
